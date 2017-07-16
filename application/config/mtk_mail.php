73a2b7d4a3a5e90005c952608260bcbde08f8bad
Özgün EŞİM <ozgunesim@gmail.com>
2017-07-16 03:45:01 +0300

    guvenlik riski

--- application/config/mtk_mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['host'] = 'smtp.gmail.com';
-$config['port'] = 456;
-$config['username'] = 'serviodestek@gmail.com';
-$config['password'] = 'ozgunceo';
-
-?>
